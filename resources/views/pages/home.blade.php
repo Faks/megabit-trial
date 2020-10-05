fcf5499964f9802b73af1f4212524d90b8b58ac6
Oskars Germovs <sia_dev@inbox.lv>
2020-10-05 12:11:51 +0300

    home.blade.php: 1.) added demo auth credentials footer.blade.php: 1.) added Bitubket, LinkedIn links

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -11,7 +11,7 @@
                                 <span class="signup">
                                     Don’t have an account?
                                 </span>
-                                
+
                                 <span class="login hidden">
                                    Have an account?
                                 </span>
@@ -21,7 +21,7 @@
                                 <span class="signup">
                                     Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                                 </span>
-                                
+
                                 <span class="login hidden">
                                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
                                 </span>
@@ -31,7 +31,7 @@
                                     <a class="btn btn-size btn-sigh-up btn-round-corners btn-vertically-centred signup card-left">
                                         Sign Up
                                     </a>
-                                    
+
                                     <a class="btn btn-size btn-sigh-up btn-round-corners btn-vertically-centred login hidden card-right">
                                         Login
                                     </a>
@@ -41,17 +41,17 @@
                     </div>
                 </div>
             </div>
-            
+
             <div class="col-6">
                 <div class="card card-animate card-megabit ml-n-30" id="card-animate">
                     <div class="card-body">
                         <div class="row">
-                            
+
                             @include('errors.errors')
                             @include('errors.success')
-    
+
                             <form class="form-right" action="/store" method="post" id="form">
-                                
+
                                 <div class="form-group row">
                                     <div class="col-sm-12">
                                         <div class="row">
@@ -60,62 +60,64 @@
                                                     <span class="signup">Login
                                                         <hr>
                                                     </span>
-                                                    
+
                                                     <span class="login hidden">Sign Up
                                                         <hr>
                                                     </span>
                                                 </h4>
                                             </div>
-                                            
+
                                             <div class="col-sm-4">
                                                 <img src="/img/logo.png">
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                                
+
                                 <div class="form-group input-group row ml-0 hidden" id="name">
                                     <label for="input-name"
                                            class="col-sm-12 col-form-label col-form-label-sm pl-0 pb-0">Name<sup
                                             class="p-1 text-danger">*</sup></label>
                                     <input type="text" name="name" class="form-control form-control-sm"
                                            id="input-name" disabled required aria-required="true">
-                                    
+
                                     <div class="input-group-append">
                                         <span class="input-group-text" id="basic-addon2">
                                             <img src="/img/ic_user_default.png">
                                         </span>
                                     </div>
                                 </div>
-                                
+
                                 <div class="form-group input-group row ml-0">
                                     <label for="input-email"
                                            class="col-sm-12 col-form-label col-form-label-sm pl-0 pb-0">Email<sup
                                             class="p-1 text-danger">*</sup></label>
                                     <input type="email" name="email" class="form-control form-control-sm"
-                                           id="input-email" required aria-required="true">
-                                    
+                                           id="input-email" required aria-required="true"
+                                           placeholder="Demo: megabit-trial@solum-designum.eu">
+
                                     <div class="input-group-append">
                                         <span class="input-group-text" id="basic-addon2">
                                             <img src="/img/ic_mail_default.png">
                                         </span>
                                     </div>
                                 </div>
-                                
+
                                 <div class="form-group input-group row ml-0">
                                     <label for="input-password"
                                            class="col-sm-12 col-form-label col-form-label-sm pl-0 pb-0">Password<sup
                                             class="p-1 text-danger">*</sup></label>
                                     <input type="password" name="password" class="form-control form-control-sm"
-                                           id="input-password" required aria-required="true">
-                                    
+                                           id="input-password" required aria-required="true"
+                                           placeholder="Demo: megabit-trial">
+
                                     <div class="input-group-append">
                                         <span class="input-group-text" id="basic-addon2">
                                             <img src="/img/ic_lock_default.png">
                                         </span>
                                     </div>
                                 </div>
-                                
+
                                 <div class="form-group row mg-t-30">
                                     <div class="col-sm-12 pr-0 pl-0">
                                         <div class="row">
@@ -128,7 +130,7 @@
                                                     </button>
                                                 </div>
                                             </div>
-                                            
+
                                             <div class="col-sm-4 pt-14">
                                                 <a href="/forgot">
                                                     <span class="signup">Forgot?</span>
@@ -152,13 +154,13 @@
         $(document).ready(function () {
             var id_selector_card_animate = $("#card-animate");
             var id_selector_card_animate_f = $("#card-animate-f");
-            
+
             $(".card-left").click(function () {
-                
+
                 $('.card-body').fadeOut(250).promise().done(function () {
                     $(this).fadeIn(1500);
                 });
-                
+
                 if (!id_selector_card_animate.hasClass('move')) {
                     id_selector_card_animate.addClass('move').animate({right: "+=430px"}, 1000);
                     id_selector_card_animate_f.animate({left: "+=460px"}, 1000);
@@ -171,15 +173,15 @@
                     // $(".card-animate").removeClass('ml-n-30');
                     $("#form").removeClass('form-right');
                     $("#form").addClass('form-left');
-                    
+
                 }
             });
-            
+
             $(".card-right").click(function () {
                 $('.card-body').fadeOut(250).promise().done(function () {
                     $(this).fadeIn(1500);
                 });
-                
+
                 id_selector_card_animate.removeClass('move').animate({right: "0"}, 1000);
                 id_selector_card_animate_f.animate({left: "0"}, 1000);
                 $(".login").addClass('hidden');
--- resources/views/template/footer.blade.php
+++ resources/views/template/footer.blade.php
@@ -1,1 +1,10 @@
 All Rights Reserved Oskars Germovs 2019.
+<br>
+<ul class="list-group list-group-horizontal list-unstyled mx-auto justify-content-center">
+    <li class="nav-item">
+        <a class="nav-link" href="https://bitbucket.org/Faks/megabit-trial/src/master/">Bitbutcket</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="https://www.linkedin.com/in/oskars-germovs-a94b3318a/">LinkedIn</a>
+    </li>
+</ul>
